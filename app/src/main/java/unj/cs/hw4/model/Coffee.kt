c6fc6e8872e3e4d9f4ea5b69454dae6878438ae9
Ahmad Luhur Pakerti <ahmadluhurp@gmail.com>
2022-10-06 22:14:49 +0700

    milestone 1

--- app/src/main/java/unj/cs/hw4/model/Coffee.kt
+++
@@ -1,9 +0,0 @@
-package unj.cs.hw4.model
-
-data class Coffee(
-    val title: Int,
-    val drawable: Int,
-    val shortDescription: Int,
-    val longDescription: Int,
-    var favorite: Boolean
-)
--- app/src/main/java/unj/cs/hw4/ui/detail/DetailViewModel.kt
+++
@@ -1,7 +0,0 @@
-package unj.cs.hw4.ui.detail
-
-import androidx.lifecycle.ViewModel
-
-class DetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
