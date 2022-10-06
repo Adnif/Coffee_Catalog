7f5b029db6cf38d9a9409540839131e95ae44627
Adnif <ahmadbani9@gmail.com>
2022-10-06 12:49:04 +0700

    hw4 commit

---
+++ app/src/main/java/unj/cs/hw4/ui/detail/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package unj.cs.hw4.ui.detail
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/unj/cs/hw4/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,41 @@
+package unj.cs.hw4.ui.favorite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.coffeeapp2.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val favoriteViewModel =
+            ViewModelProvider(this).get(FavoriteViewModel::class.java)
+
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+//        val textView: TextView = binding.textDashboard
+//        favoriteViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
