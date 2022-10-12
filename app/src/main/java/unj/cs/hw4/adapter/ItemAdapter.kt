a18ca4d4411574ff60f3f49a625710bc2aefcc1c
Ahmad Luhur Pakerti <ahmadluhurp@gmail.com>
2022-10-12 10:38:44 +0700

    Add Item Adapter Implementation

--- app/src/main/java/unj/cs/hw4/adapter/ItemAdapter.kt
+++ app/src/main/java/unj/cs/hw4/adapter/ItemAdapter.kt
@@ -1,25 +1,39 @@
 package unj.cs.hw4.adapter
 
 import android.content.Context
+import android.media.Image
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import unj.cs.hw4.R
 import unj.cs.hw4.model.Coffee
 
 class ItemAdapter(private val context: Context, private val dataset: List<Coffee>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
+        val descView: TextView = view.findViewById(R.id.item_short_description)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        TODO("Not yet implemented")
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.title)
+        holder.imageView.setImageResource(item.drawable)
+        holder.descView.text = context.resources.getString(item.shortDescription)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return dataset.size
     }
 }
--- app/src/main/java/unj/cs/hw4/model/Coffee.kt
+++ app/src/main/java/unj/cs/hw4/model/Coffee.kt
@@ -1,9 +1,13 @@
 package unj.cs.hw4.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Coffee(
     val title: Int,
     val drawable: Int,
     val shortDescription: Int,
     val longDescription: Int,
-    var favorite: Boolean
-)
+    var favorite: Boolean = false
+) : Parcelable
